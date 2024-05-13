7b3b2cdb8a1b3af28bc340d2dac47202276c54c2
h6s8n <zarjoob@gmail.com>
2024-05-13 16:06:38 +0330

    :feat: add Tailwind CSS and Flowbite setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+        "./components/**/*.{js,vue,ts}",
+        "./layouts/**/*.vue",
+        "./pages/**/*.vue",
+        "./plugins/**/*.{js,ts}",
+        "./nuxt.config.{js,ts}",
+        "./node_modules/flowbite/**/*.{js,ts}"
+    ],
+    theme: {
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            'white': '#ffffff',
+            'purple': '#3f3cbb',
+            'midnight': '#121063',
+            'metal': '#565584',
+            'tahiti': '#3ab7bf',
+            'silver': '#ecebff',
+            'bubble-gum': '#ff77e9',
+            'bermuda': '#78dcca',
+
+        },
+        extend: {},
+        plugins: [
+            require('flowbite/plugin')
+        ],
+    }
+    }
 
